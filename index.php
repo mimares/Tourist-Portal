dc834d48a980d674aea4a84b39bf85542f56e412
mimares <47007941+mimares@users.noreply.github.com>
2019-05-14 00:26:34 +0200

    Revert "preraspodela"
    This reverts commit c8178cf767e4bbbdd30ad1e9eea999d5f1845e6d.

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
                     <p>Boudin doner frankfurter pig. Cow shank bresaola pork loin tri-tip tongue venison pork belly meatloaf short loin landjaeger biltong beef ribs shankle chicken andouille.</p>
                     <a href="#" class="btn btn-common">SIGN UP</a>
                 </div>
-                <img src="assets/images/home/slider/gradskaKuca.png" class="slider-hill" alt="slider image">
-                <!-- <img src="images/home/slider/sinagoga.png" class="slider-house" alt="slider image"> -->
-                <img src="assets/images/home/slider/sun.png" class="slider-sun" alt="slider image">
-                <img src="assets/images/home/slider/birds1.png" class="slider-birds1" alt="slider image">
-                <img src="assets/images/home/slider/birds2.png" class="slider-birds2" alt="slider image">
+                <img src="images/home/slider/gradskaKuca.png" class="slider-hill" alt="slider image">
+                <!--<img src="images/home/slider/pozoriste2.png" class="slider-house" alt="slider image">  -->
+                <img src="images/home/slider/sun.png" class="slider-sun" alt="slider image">
+                <img src="images/home/slider/birds1.png" class="slider-birds1" alt="slider image">
+                <img src="images/home/slider/birds2.png" class="slider-birds2" alt="slider image">
             </div>
         </div>
     </div>
@@ -27,7 +27,7 @@
             <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="300ms">
                 <div class="single-service">
                     <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="300ms">
-                        <img src="assets/images/home/icon1.png" alt="">
+                        <img src="images/home/icon1.png" alt="">
                     </div>
                     <h2>Incredibly Responsive</h2>
                     <p>Ground round tenderloin flank shank ribeye. Hamkevin meatball swine. Cow shankle beef sirloin chicken ground round.</p>
@@ -36,7 +36,7 @@
             <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
                 <div class="single-service">
                     <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="600ms">
-                        <img src="assets/images/home/icon2.png" alt="">
+                        <img src="images/home/icon2.png" alt="">
                     </div>
                     <h2>Superior Typography</h2>
                     <p>Hamburger ribeye drumstick turkey, strip steak sausage ground round shank pastrami beef brisket pancetta venison.</p>
@@ -45,7 +45,7 @@
             <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="900ms">
                 <div class="single-service">
                     <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="900ms">
-                        <img src="assets/images/home/icon3.png" alt="">
+                        <img src="images/home/icon3.png" alt="">
                     </div>
                     <h2>Swift Page Builder</h2>
                     <p>Venison tongue, salami corned beef ball tip meatloaf bacon. Fatback pork belly bresaola tenderloin bone pork kevin shankle.</p>
@@ -82,7 +82,7 @@
         <div class="row">
             <div class="single-features">
                 <div class="col-sm-5 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
-                    <img src="assets/images/home/image1.png" class="img-responsive" alt="">
+                    <img src="images/home/image1.png" class="img-responsive" alt="">
                 </div>
                 <div class="col-sm-6 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
                     <h2>Experienced and Enthusiastic</h2>
@@ -95,12 +95,12 @@
                     <P>Mollit eiusmod id chuck turducken laboris meatloaf pork loin tenderloin swine. Pancetta excepteur fugiat strip steak tri-tip. Swine salami eiusmod sint, ex id venison non. Fugiat ea jowl cillum meatloaf.</P>
                 </div>
                 <div class="col-sm-5 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
-                    <img src="assets/images/home/image2.png" class="img-responsive" alt="">
+                    <img src="images/home/image2.png" class="img-responsive" alt="">
                 </div>
             </div>
             <div class="single-features">
                 <div class="col-sm-5 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
-                    <img src="assets/images/home/image3.png" class="img-responsive" alt="">
+                    <img src="images/home/image3.png" class="img-responsive" alt="">
                 </div>
                 <div class="col-sm-6 wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
                     <h2>Experienced and Enthusiastic</h2>
@@ -117,28 +117,28 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="clients text-center wow fadeInUp" data-wow-duration="500ms" data-wow-delay="300ms">
-                    <p><img src="assets/images/home/clients.png" class="img-responsive" alt=""></p>
+                    <p><img src="images/home/clients.png" class="img-responsive" alt=""></p>
                     <h1 class="title">Happy Clients</h1>
                     <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br> Ut enim ad minim veniam, quis nostrud </p>
                 </div>
                 <div class="clients-logo wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
                     <div class="col-xs-3 col-sm-2">
-                        <a href="#"><img src="assets/images/home/client1.png" class="img-responsive" alt=""></a>
+                        <a href="#"><img src="images/home/client1.png" class="img-responsive" alt=""></a>
                     </div>
                     <div class="col-xs-3 col-sm-2">
-                        <a href="#"><img src="assets/images/home/client2.png" class="img-responsive" alt=""></a>
+                        <a href="#"><img src="images/home/client2.png" class="img-responsive" alt=""></a>
                     </div>
                     <div class="col-xs-3 col-sm-2">
-                        <a href="#"><img src="assets/images/home/client3.png" class="img-responsive" alt=""></a>
+                        <a href="#"><img src="images/home/client3.png" class="img-responsive" alt=""></a>
                     </div>
                     <div class="col-xs-3 col-sm-2">
-                        <a href="#"><img src="assets/images/home/client4.png" class="img-responsive" alt=""></a>
+                        <a href="#"><img src="images/home/client4.png" class="img-responsive" alt=""></a>
                     </div>
                     <div class="col-xs-3 col-sm-2">
-                        <a href="#"><img src="assets/images/home/client5.png" class="img-responsive" alt=""></a>
+                        <a href="#"><img src="images/home/client5.png" class="img-responsive" alt=""></a>
                     </div>
                     <div class="col-xs-3 col-sm-2">
-                        <a href="#"><img src="assets/images/home/client6.png" class="img-responsive" alt=""></a>
+                        <a href="#"><img src="images/home/client6.png" class="img-responsive" alt=""></a>
                     </div>
                 </div>
             </div>
@@ -150,14 +150,14 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12 text-center bottom-separator">
-                <img src="assets/images/home/under.png" class="img-responsive inline" alt="">
+                <img src="images/home/under.png" class="img-responsive inline" alt="">
             </div>
             <div class="col-md-4 col-sm-6">
                 <div class="testimonial bottom">
                     <h2>Testimonial</h2>
                     <div class="media">
                         <div class="pull-left">
-                            <a href="#"><img src="assets/images/home/profile1.png" alt=""></a>
+                            <a href="#"><img src="images/home/profile1.png" alt=""></a>
                         </div>
                         <div class="media-body">
                             <blockquote>Nisi commodo bresaola, leberkas venison eiusmod bacon occaecat labore tail.</blockquote>
@@ -166,7 +166,7 @@
                     </div>
                     <div class="media">
                         <div class="pull-left">
-                            <a href="#"><img src="assets/images/home/profile2.png" alt=""></a>
+                            <a href="#"><img src="images/home/profile2.png" alt=""></a>
                         </div>
                         <div class="media-body">
                             <blockquote>Capicola nisi flank sed minim sunt aliqua rump pancetta leberkas venison eiusmod.</blockquote>
@@ -181,6 +181,7 @@
                     <address>
                         E-mail: <a href="mailto:someone@example.com">honestguidesubotica@gmail.com</a> <br>
                         Phone: +381 24 4567890 <br>
+                        Fax: +1 (123) 456 7891 <br>
                     </address>
 
                     <h2>Address</h2>
@@ -224,11 +225,11 @@
 </footer>
 <!--/#footer-->
 
-<script type="text/javascript" src="assets/js/jquery.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/js/lightbox.min.js"></script>
-<script type="text/javascript" src="assets/js/wow.min.js"></script>
-<script type="text/javascript" src="assets/js/main.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/lightbox.min.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
 
